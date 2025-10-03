404e75f7938cce49ad33277e2923a0102c559b1a
Srinath-Rajkumar <srinathrajkumar.ece@gmail.com>
2025-10-03 11:59:38 +0530

    fetching data and population dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import { useState ,useEffect } from "react";
+import { useState, useEffect } from "react";
 import DropDown from "./component/DropDown";
 
 function App() {
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("dependency_data.json");
+        const jsonData = await response.json();
+        setData(jsonData);
+        console.log(jsonData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  function findAvailableOptions(key) {
+    if (!data) {
+      return [];
+    }
+    const options = data.map((vehicle) => vehicle[key]);
+    return [...new Set(options)];
+  }
+
+  console.log("Data:", data);
+  console.log("Fuel options:", findAvailableOptions("fuel_type"));
 
   return (
     <>
       <div className="flex gap-1.5 m-2.5">
-        <DropDown label={"Fuel Type"}>Test</DropDown>
-        <DropDown label={"Make"}>Test</DropDown>
-        <DropDown label={"Model"}>Test</DropDown>
+        <DropDown
+          label={"Fuel Type"}
+          options={findAvailableOptions("fuel_type")}
+        />
+        <DropDown label="Make" options={findAvailableOptions("make")} />
+        <DropDown label="Model" options={findAvailableOptions("model")} />
       </div>
     </>
   );
--- src/component/DropDown.jsx
+++ src/component/DropDown.jsx
@@ -1,11 +1,18 @@
-function DropDown({ children, label }) {
+function DropDown({ options, label }) {
   return (
     <>
       <div className="flex flex-col w-full grow text-sm font-medium">
         <label>{label} :</label>
         <select className=" w-full flex justify-center p-2.5 border rounded-sm mt-1.5">
-          <option>Select option</option>
-          <option>{children}</option>
+          <option value="">Select {label}</option>
+          {options &&
+            options.map((option, index) => {
+              return (
+                <option value={option} key={index} >
+                  {option}
+                </option>
+              );
+            })}
         </select>
       </div>
     </>
